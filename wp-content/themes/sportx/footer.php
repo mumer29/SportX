ede9716cf564752bf38dd1395d3f6691eff2bfe5
MarioS <mario0.0somodi@gmail.com>
2021-12-12 02:12:18 +0100

    Finished front page style.

--- wp-content/themes/sportx/footer.php
+++ wp-content/themes/sportx/footer.php
@@ -1,3 +1,6 @@
+    <footer>
+        <p class="copyright text-muted">Copyright &copy; Mario Šomođi <?php echo date("Y.") ?></p>
+    </footer>
     <?php wp_footer() ?>
     </body>
 
--- wp-content/themes/sportx/front-page.php
+++ wp-content/themes/sportx/front-page.php
@@ -1,3 +1,21 @@
 <?php
 get_header();
+?>
+
+<div class="frontPageContainer">
+    <div class="row g-0">
+        <div class="col-sm d-flex align-items-center justify-content-center">
+            <h1 class="tagline pb-3 m-5">Enjoy our rich coverage of football matches, a big codex of leauges, teams,
+                venues
+                and
+                players!</h1>
+        </div>
+        <div class="col-sm d-flex align-items-center justify-content-end imgContainer">
+            <img id="image" class="img-fluid"
+                src="<?php echo get_template_directory_uri() . '/assets/img/FrontPageImg.png' ?>" alt="Football player">
+        </div>
+    </div>
+</div>
+
+<?php
 get_footer();
